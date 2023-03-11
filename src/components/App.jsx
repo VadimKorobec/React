7ce2004e5078dfe8feb5c623ff3a2ef53e455c73
VadimKorobec <_s_e_t__@ukr.net>
2023-03-11 17:55:02 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,24 @@
+import React, { useState } from 'react';
+
 export const App = () => {
-  return <div></div>;
+  const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('TEXT IN INPUT');
+
+  const increment = () => {
+    setLikes(likes + 1);
+  };
+
+  const decrement = () => {
+    setLikes(likes - 1);
+  };
+
+  return (
+    <div>
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input type="text" value={value} onChange={event => event.target.value} />
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
 };
