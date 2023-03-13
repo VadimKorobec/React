95e3931c6d44cf2c1b2e06088a549616ba815b86
VadimKorobec <_s_e_t__@ukr.net>
2023-03-13 18:20:22 +0200

    Update PostItem.jsx

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import './styles/App.css';
 
 export const PostItem = props => {
   return (
-    <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>
-            {props.post.id}.{props.post.title}
-          </strong>
-          <div>{props.post.body}</div>
-        </div>
-        <div className="post__btns">
-          <button>Delete</button>
-        </div>
+    <div className="post">
+      <div className="post__content">
+        <strong>
+          {props.post.id}.{props.post.title}
+        </strong>
+        <div>{props.post.body}</div>
+      </div>
+      <div className="post__btns">
+        <button>Delete</button>
       </div>
     </div>
   );
