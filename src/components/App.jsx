036e0d60b0c8508a2ae60b85e69452df2116322d
VadimKorobec <_s_e_t__@ukr.net>
2023-03-13 17:52:07 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { PostItem } from './PostItem';
+import './styles/App.css';
 
 export const App = () => {
   return (
-    <>
+    <div className="App">
       <PostItem post={{ id: 1, title: 'JavaScript', body: 'Description' }} />
-    </>
+      <PostItem post={{ id: 2, title: 'JavaScript', body: 'Description' }} />
+      <PostItem post={{ id: 3, title: 'JavaScript', body: 'Description' }} />
+      <PostItem post={{ id: 4, title: 'JavaScript', body: 'Description' }} />
+    </div>
   );
 };
