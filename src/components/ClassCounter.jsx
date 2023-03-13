69a345c4286bef04382c9d647d0275f3236fbd31
VadimKorobec <_s_e_t__@ukr.net>
2023-03-13 16:56:06 +0200

    Create ClassCounter.jsx

---
+++ src/components/ClassCounter.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export class ClassCounter extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            count:0,
+        }
+    }
+
+   increment() {
+        
+    }
+
+      decrement(){
+        
+        
+    }
+    render() {
+        return(<div>
+        <h1>{ count}</h1>
+        <button onClick={increment}>increment</button>
+        <button onClick={decrement}>decrement</button>
+    </div>)
+    }
+}
