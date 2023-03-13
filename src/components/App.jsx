29121fe0d66be4ada8e511977f00debabe0a345b
VadimKorobec <_s_e_t__@ukr.net>
2023-03-13 17:07:12 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Counter } from './Counter';
 
 export const App = () => {
-  
-
   return (
     <div>
-      
-      {/* <h1>{value}</h1> */}
-      {/* <input type="text" value={value} onChange={event => event.target.value} /> */}
-      <Counter/>
+      <Counter />
     </div>
   );
 };
