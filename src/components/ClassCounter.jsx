53a21ce94fcc01bfd583f4474252220257765602
VadimKorobec <_s_e_t__@ukr.net>
2023-03-13 17:50:04 +0200

    Update ClassCounter.jsx

--- src/components/ClassCounter.jsx
+++ src/components/ClassCounter.jsx
@@ -6,19 +6,23 @@ export class ClassCounter extends React.Component {
     this.state = {
       count: 0,
     };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
 
   increment() {
-    this.setState({ count: (this.state += 1) });
+    this.setState({ count: this.state.count + 1 });
   }
 
-  decrement() {}
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
   render() {
     return (
       <div>
-        <h1>{count}</h1>
-        <button onClick={increment}>increment</button>
-        <button onClick={decrement}>decrement</button>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>increment</button>
+        <button onClick={this.decrement}>decrement</button>
       </div>
     );
   }
