95395536b1e5a070e368e4b3232b010ea83f2327
VadimKorobec <_s_e_t__@ukr.net>
2023-03-16 14:20:19 +0200

    Create PostForm.jsx

---
+++ src/components/PostForm.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { MyButton } from './UI/button/MyButton';
+import { MyInput } from './UI/input/MyInput';
+
+export const PostForm = ({ create }) => {
+  const [post, setPost] = useState({ title: '', body: '' });
+
+  const addNewPost = event => {
+    event.preventDefault();
+    const newPost = {
+      ...post,
+      id: Date.now(),
+    };
+    create(newPost);
+    reset();
+  };
+
+  const reset = () => {
+    setPost({ title: '', body: '' });
+  };
+  return (
+    <div>
+      <form>
+        <MyInput
+          value={post.title}
+          onChange={event => setPost({ ...post, title: event.target.value })}
+          type="text"
+          placeholder="Titel post"
+        />
+
+        {
+          <MyInput
+            value={post.body}
+            onChange={event => setPost({ ...post, body: event.target.value })}
+            type="text"
+            placeholder="Description post"
+          />
+        }
+        <MyButton onClick={addNewPost}>Create post</MyButton>
+      </form>
+    </div>
+  );
+};
