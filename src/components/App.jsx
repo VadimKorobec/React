7d62e9d5c74d517442f0f7e39530ec4a8442e4b6
VadimKorobec <_s_e_t__@ukr.net>
2023-03-16 12:22:29 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,17 +10,14 @@ export const App = () => {
     { id: 4, title: 'Node.js', body: 'Description' },
   ]);
 
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'JavaScript', body: 'Description' },
-    { id: 2, title: 'React', body: 'Description' },
-    { id: 3, title: 'Redux', body: 'Description' },
-    { id: 4, title: 'Node.js', body: 'Description' },
-  ]);
-
   return (
     <div className="App">
+      <form>
+        <input type="text" placeholder="Titel post" />
+        <input type="text" placeholder="Description post" />
+        <button type="submit">Create post</button>
+      </form>
       <PostList posts={posts} title="List 1" />
-      <PostList posts={posts2} title="List 2" />
     </div>
   );
 };
