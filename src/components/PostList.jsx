560a2f68cb0d905bc7c171cc622b1039abf3fcda
VadimKorobec <_s_e_t__@ukr.net>
2023-03-17 13:54:31 +0200

    Update PostList.jsx

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { PostItem } from './PostItem';
 
 export const PostList = ({ posts, title, remove }) => {
+  if (!posts.length) {
+    return <h1 style={{ textAlign: 'center' }}>Posts not find</h1>;
+  }
+
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>{title}</h1>
