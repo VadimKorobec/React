384523cc5bcd1cae1446ca1a9773d0ac8b49df25
VadimKorobec <_s_e_t__@ukr.net>
2023-03-17 14:50:08 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,17 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { PostFilter } from './PostFilter';
 import { PostForm } from './PostForm';
 import { PostList } from './PostList';
 import './styles/App.css';
 import { MyButton } from './UI/button/MyButton';
 import { MyModal } from './UI/MyModal/MyModal';
+import { usePosts } from 'hooks/usePosts';
 
 export const App = () => {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'JavaScript', body: 'Description' },
-    { id: 2, title: 'React', body: 'Description' },
-    { id: 3, title: 'Redux', body: 'Description' },
-    { id: 4, title: 'Node.js', body: 'Description' },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchPosts = useMemo(() => {
-    return sortedPosts.filter(post =>
-      post.title.toLowerCase().includes(filter.query)
-    );
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = newPost => {
     setPosts([...posts, newPost]);
