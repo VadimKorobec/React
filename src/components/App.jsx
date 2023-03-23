edcb162fe2ca21e7f20844c35047bc8f5d07b715
VadimKorobec <_s_e_t__@ukr.net>
2023-03-23 15:09:45 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,8 +19,6 @@ export const App = () => {
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
 
-  let pagesArray = getPagesArray(totalPages);
-
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
     const response = await PostService.getAll(limit, page);
     setPosts(response.data);
@@ -76,17 +74,6 @@ export const App = () => {
           title="List"
         />
       )}
-      <div className="page__wrapper">
-        {pagesArray.map(p => (
-          <span
-            onClick={() => changePage(p)}
-            key={p}
-            className={page === p ? 'page__current' : 'page'}
-          >
-            {p}
-          </span>
-        ))}
-      </div>
     </div>
   );
 };
