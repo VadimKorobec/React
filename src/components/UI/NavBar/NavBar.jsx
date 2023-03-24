50d50635c996623c3816b08510e6c2c8d83a7f57
VadimKorobec <_s_e_t__@ukr.net>
2023-03-24 17:55:12 +0200

    Update NavBar.jsx

--- src/components/UI/NavBar/NavBar.jsx
+++ src/components/UI/NavBar/NavBar.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
-    <div className="navbar">
-      <div className="navbar__link">
-        <Link to="/about">About</Link>
-        <Link to="/posts">Posts</Link>
+    <div>
+      <div className="navbar">
+        <div className="navbar__link">
+          <Link to="/about">About</Link>
+          <Link to="/posts">Posts</Link>
+        </div>
       </div>
     </div>
   );
