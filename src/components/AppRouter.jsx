ad3aa2b285e70ece8bca01f91b0deb3d7872b5e1
VadimKorobec <_s_e_t__@ukr.net>
2023-03-24 17:15:46 +0200

    Create AppRouter.jsx

---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,16 @@
+import { About } from 'pages/About';
+import { NotFound } from 'pages/NotFound';
+import { Posts } from 'pages/Posts';
+import { Routes, Route } from 'react-router-dom';
+
+export const AppRouter = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+};
