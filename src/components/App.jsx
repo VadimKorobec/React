7ed11c9a72d329ea325ac6460044aefd47841060
VadimKorobec <_s_e_t__@ukr.net>
2023-03-24 17:15:48 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,13 @@
-import { About } from 'pages/About';
-import { Posts } from 'pages/Posts';
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import './styles/App.css';
+import { AppRouter } from './AppRouter';
+import { NavBar } from './UI/NavBar/NavBar';
 
 export const App = () => {
   return (
     <>
-      <div className="navbar">
-        <div className="navbar__link">
-          <a href="/about">About</a>
-          <a href="/posts">Posts</a>
-        </div>
-      </div>
-      <Routes>
-        <Route path="/about" element={<About />} />
-
-        <Route path="/posts" element={<Posts />} />
-      </Routes>
+      <NavBar />
+      <AppRouter />
     </>
   );
 };
